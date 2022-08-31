fb047a25d638a69456831eeb471cf45cce27d7ee
kaua pereira tavares <kptgit@gmail.com>
2022-08-31 08:31:15 -0300

    commit2

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, TextInput } from 'react-native';
+
+const oi = () =>{
+  return (
+    <ScrollView>
+      <View>
+        <Text
+    style={{
+      fontsize: 45,
+      alignSelf: "center",
+      color:"green",
+    }}
+    >
+    teste
+    </Text>
+    {/* <Image source={{URL"https://www.magazineluiza.com.br/adaptador-wireless-usb-2-0-d-link-dwa-123/p/hf4cf3c585/in/apwf/"}}> </Image>  */}
+    </View>
+    </ScrollView>
+  )};
+  
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>AII QUE LINDOOOOOOOOOOOOOOOOOO</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
